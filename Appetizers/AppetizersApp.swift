a2db9777c5d16a425aec9105db7fc6e5d4e2f457
OvershotHavik <plavetzky@gmail.com>
2021-11-15 09:57:31 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Steve Plavetzky on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
